73f1151ba39b02488c41631fb5814e73cd48a723
Ro-Dunn <minutetoominute@gmail.com>
2022-08-18 08:58:51 -0600

    BAsicSetUp

--- WriteItOut/ContentView.swift
+++ WriteItOut/ContentView.swift
@@ -2,18 +2,30 @@
 //  ContentView.swift
 //  WriteItOut
 //
-//  Created by Katy Dunn on 8/18/22.
+//  Created by Ro Dunn on 8/18/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button ("Start") {
+                    print("Start Pressed")
+                }
+//                Button ("Color Map") {
+//                    print("Color Map Pressed")
+//                }
+            }
+            .toolbar {
+                Button("User") {
+                    print("User Pressed")
+                }
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
