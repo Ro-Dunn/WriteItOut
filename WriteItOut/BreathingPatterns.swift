99db9fb627f286c896ba22fad14874ae90665aba
Ro-Dunn <minutetoominute@gmail.com>
2022-08-18 09:25:51 -0600

    UserSetUp

---
+++ WriteItOut/BreathingPatterns.swift
@@ -0,0 +1,8 @@
+//
+//  BreathingPatterns.swift
+//  WriteItOut
+//
+//  Created by Katy Dunn on 8/18/22.
+//
+
+import Foundation
---
+++ WriteItOut/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  WriteItOut
+//
+//  Created by Katy Dunn on 8/18/22.
+//
+
+import Foundation
