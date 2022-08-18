4a5866afa68b74180e81e2186a2d1a538fdbada8
Ro-Dunn <minutetoominute@gmail.com>
2022-08-18 08:02:01 -0600

    Initial Commit

---
+++ WriteItOut/WriteItOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriteItOutApp.swift
+//  WriteItOut
+//
+//  Created by Katy Dunn on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WriteItOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
